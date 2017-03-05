c219c8c2a9015b116681628d3701d9053107fcc3
Trevor Laughlin <trevor@laughlinresearch.com>
2017-03-05 09:43:35 -0500

    Basic smesh integration

--- asap/fem/hypotheses.py
+++ asap/fem/hypotheses.py
@@ -12,12 +12,12 @@ class Hypothesis(object):
     """
     Base class for all hypotheses.
     """
-    _hypos = {}
+    _all = {}
     _indx = 0
 
     def __init__(self, name, smesh_hypo):
         self._name = name
-        self._hypos[name] = self
+        Hypothesis._all[name] = self
         self._smesh_hypo = smesh_hypo(Hypothesis._indx, 0, _mesh_gen)
         self._id = Hypothesis._indx
         Hypothesis._indx += 1
@@ -30,91 +30,221 @@ class Hypothesis(object):
     def id(self):
         return self._id
 
+    @classmethod
+    def get_hypothesis(cls, hypothesis):
+        """
+        Get a hypothesis.
 
-class Regular1d(Hypothesis):
+        :param hypothesis:
+
+        :return:
+        """
+        if isinstance(hypothesis, Hypothesis):
+            return hypothesis
+        try:
+            return Hypothesis._all[hypothesis]
+        except KeyError:
+            return None
+
+
+class Regular1D(Hypothesis):
     """
     Regular 1-D algorithm
     """
 
     def __init__(self, name):
         smesh_hypo = StdMeshers_Regular_1D
-        super(Regular1d, self).__init__(name, smesh_hypo)
+        super(Regular1D, self).__init__(name, smesh_hypo)
 
 
-class MaxLength1d(Hypothesis):
+class MaxLength1D(Hypothesis):
     """
     Maximum length 1-D hypothesis.
     """
 
     def __init__(self, name, max_length):
         smesh_hypo = StdMeshers_MaxLength
-        super(MaxLength1d, self).__init__(name, smesh_hypo)
+        super(MaxLength1D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetLength(max_length)
 
 
-class LocalLength1d(Hypothesis):
+class LocalLength1D(Hypothesis):
     """
     Local length 1-D hypothesis.
     """
 
     def __init__(self, name, local_length):
         smesh_hypo = StdMeshers_LocalLength
-        super(LocalLength1d, self).__init__(name, smesh_hypo)
+        super(LocalLength1D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetLength(local_length)
 
 
-class NumberOfSegments1d(Hypothesis):
+class NumberOfSegments1D(Hypothesis):
     """
     Number of segments 1-D hypothesis.
     """
 
     def __init__(self, name, nseg):
         smesh_hypo = StdMeshers_NumberOfSegments
-        super(NumberOfSegments1d, self).__init__(name, smesh_hypo)
+        super(NumberOfSegments1D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetNumberOfSegments(nseg)
 
 
-class Adaptive1d(Hypothesis):
+class Adaptive1D(Hypothesis):
     """
     Adaptive length 1-D hypothesis.
     """
 
     def __init__(self, name, min_size, max_size, deflection):
         smesh_hypo = StdMeshers_Adaptive1D
-        super(Adaptive1d, self).__init__(name, smesh_hypo)
+        super(Adaptive1D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetMinSize(min_size)
         self.smesh_obj.SetMaxSize(max_size)
         self.smesh_obj.SetDeflection(deflection)
 
 
-class Deflection1d(Hypothesis):
+class Deflection1D(Hypothesis):
     """
     Deflection length 1-D hypothesis.
     """
 
     def __init__(self, name, deflection):
         smesh_hypo = StdMeshers_Deflection1D
-        super(Deflection1d, self).__init__(name, smesh_hypo)
+        super(Deflection1D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetDeflection(deflection)
 
 
-class NetgenSimple2d(Hypothesis):
+class NetgenSimple2D(Hypothesis):
     """
     Netgen 2-D simple hypothesis.
     """
 
     def __init__(self, name, local_length, allow_quads=True):
         smesh_hypo = NETGENPlugin_SimpleHypothesis_2D
-        super(NetgenSimple2d, self).__init__(name, smesh_hypo)
+        super(NetgenSimple2D, self).__init__(name, smesh_hypo)
         self.smesh_obj.SetLocalLength(local_length)
         self.smesh_obj.SetAllowQuadrangles(allow_quads)
 
 
-class NetgenAlgo2d(Hypothesis):
+class NetgenAlgo2D(Hypothesis):
     """
     Netgen 2-D algorithm.
     """
 
     def __init__(self, name):
         smesh_hypo = NETGENPlugin_NETGEN_2D
-        super(NetgenAlgo2d, self).__init__(name, smesh_hypo)
+        super(NetgenAlgo2D, self).__init__(name, smesh_hypo)
+
+
+class HypothesisData(object):
+    """
+    Hypothesis data manager.
+    """
+
+    @staticmethod
+    def get_hypothesis(hypothesis):
+        """
+        Get a hypothesis.
+
+        :param hypothesis:
+
+        :return:
+        """
+        return Hypothesis.get_hypothesis(hypothesis)
+
+    @staticmethod
+    def create_regular_1d(name):
+        """
+        Create a Regular1D hypothesis.
+
+        :param str name:
+
+        :return:
+        """
+        return Regular1D(name)
+
+    @staticmethod
+    def create_max_length_1d(name, max_length):
+        """
+        Create a MaxLength1D hypothesis.
+
+        :param name:
+        :param max_length:
+
+        :return:
+        """
+        return MaxLength1D(name, max_length)
+
+    @staticmethod
+    def create_local_length_1d(name, local_length):
+        """
+        Create a LocalLength1D hypothesis.
+
+        :param name:
+        :param local_length:
+
+        :return:
+        """
+        return LocalLength1D(name, local_length)
+
+    @staticmethod
+    def create_number_of_segments_1d(name, nseg):
+        """
+        Create a NumberOfSegments1D hypothesis.
+
+        :param name:
+        :param nseg:
+
+        :return:
+        """
+        return NumberOfSegments1D(name, nseg)
+
+    @staticmethod
+    def create_adaptive_1d(name, min_size, max_size, deflection):
+        """
+        Create an Adaptive1D hypothesis.
+
+        :param name:
+        :param min_size:
+        :param max_size:
+        :param deflection:
+
+        :return:
+        """
+        return Adaptive1D(name, min_size, max_size, deflection)
+
+    @staticmethod
+    def create_deflection_1d(name, deflection):
+        """
+        Create Deflection1D hypothesis.
+
+        :param name:
+        :param deflection:
+
+        :return:
+        """
+        return Deflection1D(name, deflection)
+
+    @staticmethod
+    def create_netgen_simple_2d(name, local_length, allow_quads=True):
+        """
+        Create a NetgenSimple2D hypothesis.
+
+        :param name:
+        :param local_length:
+        :param allow_quads:
+
+        :return:
+        """
+        return NetgenSimple2D(name, local_length, allow_quads)
+
+    @staticmethod
+    def create_netgen_algo_2d(name):
+        """
+        Create NetgenAlgo2D hypothesis.
+
+        :param name:
+
+        :return:
+        """
+        return NetgenAlgo2D(name)
--- asap/fem/meshes.py
+++ asap/fem/meshes.py
@@ -1,5 +1,6 @@
 from OCC.SMESH import SMESH_Gen_get
 
+from .hypotheses import HypothesisData
 from ..topology import ShapeTools
 
 _mesh_gen = SMESH_Gen_get()
@@ -9,13 +10,14 @@ class Mesh(object):
     """
     Mesh class.
     """
-    _meshes = {}
+    _all = {}
     _indx = 0
 
     def __init__(self, name):
         self._name = name
-        self._meshes[name] = self
+        Mesh._all[name] = self
         self._smesh_mesh = _mesh_gen.CreateMesh(Mesh._indx, True)
+        self._smesh_ds = self._smesh_mesh.GetMeshDS()
         self._id = Mesh._indx
         Mesh._indx += 1
 
@@ -28,8 +30,57 @@ class Mesh(object):
         return self._smesh_mesh.GetShapeToMesh()
 
     @property
+    def has_shape(self):
+        return self._smesh_mesh.HasShapeToMesh()
+
+    @property
     def smesh_obj(self):
         return self._smesh_mesh
+
+    @property
+    def nb_nodes(self):
+        return self._smesh_ds.NbNodes()
+
+    @property
+    def min_node_id(self):
+        return self._smesh_ds.MinNodeID()
+
+    @property
+    def max_node_id(self):
+        return self._smesh_ds.MaxNodeID()
+
+    @property
+    def min_elm_id(self):
+        return self._smesh_ds.MinElementID()
+
+    @property
+    def max_elm_id(self):
+        return self._smesh_ds.MaxElementID()
+
+    @classmethod
+    def get_mesh(cls, mesh=None):
+        """
+        Get a mesh.
+
+        :param mesh:
+
+        :return:
+        """
+        if isinstance(mesh, Mesh):
+            return mesh
+        try:
+            return Mesh._all[mesh]
+        except KeyError:
+            return None
+
+    def activate(self):
+        """
+        Activate this mesh.
+
+        :return:
+        """
+        MeshData._active = self
+        return True
 
     def shape_to_mesh(self, shape):
         """
@@ -45,17 +96,26 @@ class Mesh(object):
         self._smesh_mesh.ShapeToMesh(shape)
         return True
 
-    def add_hypotheses(self, shape, *hypotheses):
+    def add_hypothesis(self, hypothesis, shape=None):
         """
-        Add hypotheses to the shape.
+        Add a hypothesis to the shape.
 
-        :param hypotheses:
+        :param hypothesis:
         :param shape:
 
         :return:
         """
-        for hypo in hypotheses:
-            self._smesh_mesh.AddHypothesis(shape, hypo.id)
+        shape = ShapeTools.to_shape(shape)
+        if not shape:
+            if self.has_shape:
+                shape = self.shape
+            else:
+                return False
+        hypothesis = HypothesisData.get_hypothesis(hypothesis)
+        if not hypothesis:
+            return False
+        self._smesh_mesh.AddHypothesis(shape, hypothesis.id)
+        return True
 
     def compute(self):
         """
@@ -65,10 +125,118 @@ class Mesh(object):
         """
         return _mesh_gen.Compute(self._smesh_mesh, self.shape)
 
+    def clear(self):
+        """
+        Clear all nodes and elements.
+
+        :return:
+        """
+        self._smesh_mesh.Clear()
+        return True
+
+    def get_submesh(self, subshape):
+        """
+        Get a SubMesh from a subshape.
+
+        :param subshape:
+
+        :return:
+        """
+        # TODO Get SubMeshes.
+
 
 class SubMesh(object):
     pass
 
 
 class MeshData(object):
-    pass
+    """
+    Mesh data manager.
+    """
+    _active = None
+    hypotheses = HypothesisData()
+
+    @classmethod
+    def get_active(cls):
+        """
+        Get the active mesh.
+
+        :return:
+        """
+        return cls._active
+
+    @classmethod
+    def get_mesh(cls, mesh=None):
+        """
+        Get mesh.
+
+        :param mesh:
+
+        :return:
+        """
+        mesh = Mesh.get_mesh(mesh)
+        if mesh:
+            return mesh
+        return cls._active
+
+    @classmethod
+    def make_active(cls, mesh):
+        """
+        Activate the mesh.
+
+        :param mesh:
+
+        :return:
+        """
+        mesh = cls.get_mesh(mesh)
+        if not mesh:
+            return False
+        mesh.activate()
+        return True
+
+    @classmethod
+    def create_mesh(cls, name, shape, active=True):
+        """
+        Create a mesh
+
+        :param name:
+        :param shape:
+        :param active:
+
+        :return:
+        """
+        mesh = Mesh(name)
+        mesh.shape_to_mesh(shape)
+        if active:
+            mesh.activate()
+        return mesh
+
+    @classmethod
+    def add_hypothesis(cls, hypothesis, shape=None, mesh=None):
+        """
+        Add a hypothesis to the shape in a mesh.
+
+        :param hypothesis:
+        :param shape:
+        :param mesh:
+
+        :return:
+        """
+        mesh = cls.get_mesh(mesh)
+        if not mesh:
+            return False
+        return mesh.add_hypothesis(hypothesis, shape)
+
+    @classmethod
+    def compute_mesh(cls, mesh=None):
+        """
+        Compute a mesh.
+
+        :param mesh:
+
+        :return:
+        """
+        mesh = cls.get_mesh(mesh)
+        if not mesh:
+            return False
+        return mesh.compute()
