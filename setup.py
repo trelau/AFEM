e96b408056cedaaa1bdd8614e556e6f5d15a9ccf
Trevor Laughlin <trevor@laughlinresearch.com>
2017-03-14 18:43:50 -0400

    Update package install and setup files

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@ kwds = {'name': 'asap',
         'version': '0.0.1',
         'packages': find_packages('.'),
         'package_dir': {'asap': 'asap'},
-        'include_package_data': True,
         'author': 'Laughlin Research, LLC',
         'author_email': 'trevor@laughlinresearch.com',
         'maintainer': 'Laughlin Research, LLC',
         'maintainer_email': 'trevor@laughlinresearch.com',
         'description': 'Aerospace Structural Analysis Program',
-        'license': 'Proprietary',
-        'zip_safe': True}
+        'license': 'Proprietary'}
 
 setup(**kwds)
