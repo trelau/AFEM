0e7b146c18f771fe0e8e7cf77460396a9b5bf8fa
Trevor Laughlin <trevor@laughlinresearch.com>
2017-05-15 11:42:36 -0400

    Remove TODO's in main __init__

--- asap/__init__.py
+++ asap/__init__.py
@@ -1,6 +1,1 @@
-# TODO List of items to overhaul.
-# Use OCC methods and parameters in core geometry methods.
-# Use shapes and BRepAdaptor curves for wing parts. Attach ref geom as needed.
-# Use shape and BRepAdaptor in wing reference surface.
-# Do above two items to improve robustness and retain tolerance.
 from .patches import *
