ca38b5739284621b7723a269f9e41857e600431d
Trevor Laughlin <trevor@laughlinresearch.com>
2017-07-31 15:22:21 -0400

    Add create module in structure

---
+++ afem/structure/create.py
@@ -0,0 +1,102 @@
+class SurfacePartByShape(object):
+    pass
+
+
+class CurvePartByShape(object):
+    pass
+
+
+class SparByParameters(object):
+    pass
+
+
+class SparByPoints(object):
+    pass
+
+
+class SparByShape(object):
+    pass
+
+
+class SparBetweenGeom(object):
+    pass
+
+
+class SparsBetweenPlanes(object):
+    pass
+
+
+class SparsAtShapes(object):
+    pass
+
+
+class SparsAlongCurve(object):
+    pass
+
+
+class RibByParameters(object):
+    pass
+
+
+class RibByPoints(object):
+    pass
+
+
+class RibByShape(object):
+    pass
+
+
+class RibBetweenGeom(object):
+    pass
+
+
+class RibsBetweenPlanes(object):
+    pass
+
+
+class RibsAtShapes(object):
+    pass
+
+
+class RibsAlongCurve(object):
+    pass
+
+
+class BulkheadByShape(object):
+    pass
+
+
+class FloorByShape(object):
+    pass
+
+
+class FrameByShape(object):
+    pass
+
+
+class FramesBetweenPlanes(object):
+    pass
+
+
+class FramesAtShapes(object):
+    pass
+
+
+class SkinBySolid(object):
+    pass
+
+
+class SkinByBody(object):
+    pass
+
+
+class StringerBySpine(object):
+    pass
+
+
+class StringerBySection(object):
+    pass
+
+
+class StringersBySections(object):
+    pass
