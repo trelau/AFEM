6a98abac27c1db980a4c1ae4bae7451385a7853c
Trevor Laughlin <trevor@laughlinresearch.com>
2017-08-11 10:16:09 -0400

    Set version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='afem',
-        version='0.1.0',
+        version='0.2.0',
         packages=find_packages('.'),
         package_dir={'afem': 'afem'},
         author='Laughlin Research, LLC',
