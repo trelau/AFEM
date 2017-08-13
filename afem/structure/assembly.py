73ba6c2b6d61ebca8f1418d4061dddd8306e77c9
Trevor Laughlin <trevor@laughlinresearch.com>
2017-08-13 13:09:38 -0400

    Finish docs for assemblies and set name of master assembly to `_master`

--- afem/structure/assembly.py
+++ afem/structure/assembly.py
@@ -6,10 +6,15 @@ __all__ = ["Assembly", "AssemblyAPI"]
 
 class Assembly(object):
     """
-    Structural assembly.
+    Structural assembly. This is a minimal implementation only to help
+    organize parts during the construction process.
+
+    :param str label: The label.
+    :param parent: The parent assembly, if any.
+    :type parent: afem.structure.assembly.Assembly or None
     """
 
-    def __init__(self, label, parent):
+    def __init__(self, label, parent=None):
         self._label = label
         self._parent = parent
         self._children = set()
@@ -20,18 +25,34 @@ class Assembly(object):
 
     @property
     def label(self):
+        """
+        :return: The label.
+        :rtype: str
+        """
         return self._label
 
     @property
     def parent(self):
+        """
+        :return: The parent assembly, if any.
+        :rtype: afem.structure.assembly.Assembly or None
+        """
         return self._parent
 
     @property
     def parts(self):
+        """
+        :return: List of all parts.
+        :rtype: list[afem.structure.entities.Part]
+        """
         return list(self._parts)
 
     @property
     def elements(self):
+        """
+        :return: The elements of all parts.
+        :rtype: set(afem.fem.elements.Element)
+        """
         elm_set = set()
         for part in self.parts:
             elm_set.update(part.elements)
@@ -39,6 +60,10 @@ class Assembly(object):
 
     @property
     def nodes(self):
+        """
+        :return: The nodes of all parts.
+        :rtype: set(afem.fem.nodes.Node)
+        """
         node_set = set()
         for part in self.parts:
             node_set.update(part.nodes)
@@ -46,38 +71,40 @@ class Assembly(object):
 
     @property
     def metadata(self):
+        """
+        :return: The metadata dictionary.
+        :rtype: dict
+        """
         return self._metadata
 
     def add_metadata(self, key, value):
         """
         Add metadata to the assembly.
 
-        :param key:
-        :param value:
+        :param key: The key.
+        :param value: The value.
 
-        :return:
+        :return: None.
         """
         self._metadata[key] = value
-        return True
 
     def get_metadata(self, key):
         """
         Get metadata.
 
-        :param key:
+        :param key: The key.
 
-        :return:
+        :return: The value.
+
+        :raise KeyError: If the key is not in the dictionary.
         """
-        try:
-            return self._metadata[key]
-        except KeyError:
-            return None
+        return self._metadata[key]
 
     def activate(self):
         """
         Activate this assembly.
 
-        :return:
+        :return: None
         """
         AssemblyAPI._active = self
 
@@ -85,34 +112,43 @@ class Assembly(object):
         """
         Add parts to the assembly.
 
-        :param parts: Parts to add.
+        :param afem.structure.entities.Part parts: Part(s) to add.
+
+        :return: None.
         """
         part_set = set(parts)
         self._parts.update(part_set)
 
     def get_part(self, label):
         """
-        Get a part in the assembly by name.
+        Get a part in the assembly by label.
 
-        :param label: Part label.
+        :param str label: Part label.
 
-        :return: Part or *None* if part is not found.
+        :return: The part.
+        :rtype: afem.structure.entities.Part
+
+        :raise KeyError: If the part is not found.
         """
         for part in self.parts:
             if part.label == label:
                 return part
-        return None
+        raise KeyError('Part with given label could not be found in the '
+                       'assembly.')
 
     def get_parts(self, include_subassy=True, rtype=None, order=False):
         """
         Get all the parts from the assembly and its sub-assemblies.
 
-        :param include_subassy:
-        :param afem.structure.entities.Part rtype: Option to return only
-            parts of a certain type.
+        :param bool include_subassy: Option to recursively include parts
+            from any sub-assemblies.
+        :param rtype: Option to return only parts of a certain type. Provide a
+            class to check if the part is of the given type using
+            *isinstance()*.
         :param bool order: Option to order parts by their ID.
 
-        :return:
+        :return: List of parts.
+        :rtype: list[afem.structure.entities.Part]
         """
         parts = []
         for part in self.parts:
@@ -133,11 +169,14 @@ class Assembly(object):
     def prepare_shape_to_mesh(self, include_subassy=True):
         """
         Prepare a shape to mesh using the parts in the assembly and its
-        sub-assemblies.
+        sub-assemblies. This puts all the parts into a single compound which
+        can be used as the master shape for the meshing process.
 
-        :param include_subassy:
+        :param bool include_subassy: Option to recursively include parts
+            from any sub-assemblies.
 
-        :return:
+        :return: The parts as a compound.
+        :rtype: OCC.TopoDS.TopoDS_Compound
         """
         parts = self.get_parts(include_subassy)
         if not parts:
@@ -147,11 +186,25 @@ class Assembly(object):
 
 class AssemblyAPI(object):
     """
-    Assembly API.
+    Assembly API. This stores all created assemblies so data can be accessed
+    from one place. There is always a master model named '_master' that is
+    created at initialization. This will be the parent of all assemblies
+    that are not provided a parent when created. No assemblies should be
+    labeled '_master'.
     """
-    _master = Assembly('Model', None)
-    _all = {'Model': _master}
+    _master = Assembly('_master', None)
+    _all = {'_master': _master}
     _active = _master
+
+    @classmethod
+    def get_master(cls):
+        """
+        Get the master assembly.
+
+        :return: The master assembly.
+        :rtype: afem.structure.assembly.Assembly
+        """
+        return cls._master
 
     @classmethod
     def get_active(cls):
@@ -159,7 +212,7 @@ class AssemblyAPI(object):
         Get the active assembly.
 
         :return: Active assembly.
-        :rtype: :class:`.Assembly`
+        :rtype: afem.structure.assembly.Assembly
         """
         assy = cls._active
         if not isinstance(assy, Assembly):
@@ -167,46 +220,57 @@ class AssemblyAPI(object):
         return assy
 
     @classmethod
-    def make_active(cls, assy):
-        """
-        Activate the assembly.
-
-        :param assy:
-        :return:
-        """
-        assy = cls.get_assy(assy)
-        assy.activate()
-
-    @classmethod
     def get_assy(cls, assy=None):
         """
-        Get assembly.
+        Get an assembly. If a string is provided then the assembly label
+        will be used to get the assembly. If an assembly instance is
+        provided then it is simply returned. If ``None`` is provided then
+        the active assembly is returned.
 
-        :param assy: Assembly name to get. If an assembly instance is
-            provided, then it is returned.
-        :return: Assembly or active assembly if name does not exist.
-        :rtype: :class:`.Assembly`
+        :param assy: Assembly to get.
+        :type assy: str or afem.structure.assembly.Assembly or None
+
+        :return: The assembly.
+        :rtype: afem.structure.assembly.Assembly
         """
-        if assy is False:
-            return None
         if isinstance(assy, Assembly):
             return assy
+
         try:
             return cls._all[assy]
         except KeyError:
             return cls.get_active()
 
     @classmethod
+    def make_active(cls, assy):
+        """
+        Activate the assembly. This method uses the *AssemblyAPI.get_assy()*
+        method to get the assembly to activate.
+
+        :param assy: Assembly to activate.
+        :type assy: str or afem.structure.assembly.Assembly or None
+
+        :return: None.
+        """
+        assy = cls.get_assy(assy)
+        assy.activate()
+
+    @classmethod
     def create_assy(cls, label, parent=None, active=True, *parts):
         """
         Create an assembly.
 
-        :param label:
-        :param parent:
-        :param active:
-        :param parts:
+        :param str label: The label.
+        :param parent: The parent assembly. If ``None`` then the active
+            assembly is used.
+        :type parent: str or afem.structure.assembly.Assembly or None
+        :param bool active: Option to make the new assembly the active one.
 
-        :return:
+        :param afem.structure.entities.Part parts: The part(s) to add to the
+            assembly, if any.
+
+        :return: New assembly.
+        :rtype: afem.structure.assembly.Assembly
         """
         if label in cls._all:
             return None
@@ -226,30 +290,32 @@ class AssemblyAPI(object):
         """
         Add parts to the assembly.
 
-        :param assy:
-        :param parts:
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used.
+        :type assy: str or afem.structure.assembly.Assembly or None
+        :param afem.structure.entities.Part parts: The part(s) to add.
 
-        :return:
+        :return: None.
         """
         assy = cls.get_assy(assy)
-        if not isinstance(assy, Assembly):
-            return False
         assy.add_parts(*parts)
-        return True
 
     @classmethod
     def get_part(cls, label, assy=None):
         """
-        Get a part from the assembly.
+        Get a part from the assembly using its label.
         
-        :param label: 
-        :param assy:
+        :param str label: The part label.
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used.
+        :type assy: str or afem.structure.assembly.Assembly or None
          
-        :return: 
+        :return: The part.
+        :rtype: afem.structure.entities.Part
+
+        :raise KeyError: If the part is not found.
         """
         assy = cls.get_assy(assy)
-        if not isinstance(assy, Assembly):
-            return None
         return assy.get_part(label)
 
     @classmethod
@@ -258,51 +324,55 @@ class AssemblyAPI(object):
         """
         Get parts from assembly.
 
-        :param assy:
-        :param bool include_subassy:
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used.
+        :type assy: str or afem.structure.assembly.Assembly or None
+        :param bool include_subassy: Option to recursively include parts
+            from any sub-assemblies.
         :param rtype: Option to return only parts of a certain type. Provide a
-            class to check if the part is of the given type.
+            class to check if the part is of the given type using
+            *isinstance()*.
         :param bool order: Option to order parts by their ID.
 
         :return: The parts.
         :rtype: list[afem.structure.entities.Part]
         """
         assy = cls.get_assy(assy)
-        if not isinstance(assy, Assembly):
-            return []
         return assy.get_parts(include_subassy, rtype, order)
 
     @classmethod
-    def prepare_shape_to_mesh(cls, assy=None):
+    def prepare_shape_to_mesh(cls, assy='_master', include_subassy=True):
         """
-        Prepare a shape for meshing.
+        Prepare a shape to mesh using the parts in the assembly and its
+        sub-assemblies. This puts all the parts into a single compound which
+        can be used as the master shape for the meshing process.
 
-        :param assy:
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used. By default the master model is used.
+        :type assy: str or afem.structure.assembly.Assembly or None
+        :param bool include_subassy: Option to recursively include parts
+            from any sub-assemblies.
 
-        :return:
+        :return: The parts as a compound.
+        :rtype: OCC.TopoDS.TopoDS_Compound
         """
-        if assy is None:
-            assy = cls._master
-        else:
-            assy = cls.get_assy(assy)
-        if not assy:
-            return None
-        return assy.prepare_shape_to_mesh()
+        assy = cls.get_assy(assy)
+        return assy.prepare_shape_to_mesh(include_subassy)
 
     @classmethod
     def add_metadata(cls, key, value, assy=None):
         """
         Add metadata to the assembly.
 
-        :param key:
-        :param value:
-        :param assy:
+        :param key: The key.
+        :param value: The value.
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used.
+        :type assy: str or afem.structure.assembly.Assembly or None
 
-        :return:
+        :return: None.
         """
         assy = cls.get_assy(assy)
-        if not isinstance(assy, Assembly):
-            return False
         return assy.add_metadata(key, value)
 
     @classmethod
@@ -310,12 +380,14 @@ class AssemblyAPI(object):
         """
         Get metadata.
 
-        :param key:
-        :param assy:
+        :param key: They key.
+        :param assy: The assembly. If ``None`` then the active assembly is
+            used.
+        :type assy: str or afem.structure.assembly.Assembly or None
 
-        :return:
+        :return: The value.
+
+        :raise KeyError: If the key is not in the dictionary.
         """
         assy = cls.get_assy(assy)
-        if not isinstance(assy, Assembly):
-            return None
         return assy.get_metadata(key)
