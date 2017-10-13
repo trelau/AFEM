917834afe4a1c8259140dad8bb8f167f3fa1e703
Trevor Laughlin <trevor@laughlinresearch.com>
2017-10-12 22:26:15 -0400

    Simple wing example for annual review

---
+++ examples/simple_wing.py
@@ -0,0 +1,44 @@
+from afem.config import Settings
+from afem.fem import MeshAPI
+from afem.graphics import Viewer
+from afem.io import ImportVSP
+from afem.structure import *
+
+Settings.log_to_console(True)
+
+# Set units to inch.
+Settings.set_units('in')
+
+# Import model
+fn = r'../models/simple_wing.stp'
+ImportVSP.step_file(fn)
+wing = ImportVSP.get_body('WingGeom')
+
+# Build structure
+wingbox = AssemblyAPI.create_assy('wing box')
+fspar = SparByParameters('front spar', 0.15, 0., 0.15, 1., wing).spar
+rspar = SparByParameters('rear spar', 0.70, 0., 0.70, 1., wing).spar
+RibByPoints('root rib', fspar.p1, rspar.p1, wing)
+RibByPoints('tip rib', fspar.p2, rspar.p2, wing)
+RibsAlongCurveByDistance('rib', rspar.cref, 30, fspar, rspar, wing, d1=30, d2=-30)
+internal_parts = wingbox.get_parts()
+skin = SkinByBody('skin', wing).skin
+FuseSurfaceParts([skin], internal_parts)
+
+skin.set_transparency(0.75)
+Viewer.add(*wingbox.parts)
+Viewer.show(False)
+
+# Mesh
+the_shape = wingbox.prepare_shape_to_mesh()
+MeshAPI.create_mesh('the mesh', the_shape)
+
+# Unstructured quad-dominant
+MeshAPI.hypotheses.create_netgen_simple_2d('netgen', 4.)
+MeshAPI.hypotheses.create_netgen_algo_2d('netgen algo')
+MeshAPI.add_hypothesis('netgen')
+MeshAPI.add_hypothesis('netgen algo')
+MeshAPI.compute_mesh()
+
+Viewer.add_meshes(MeshAPI.get_active())
+Viewer.show()
