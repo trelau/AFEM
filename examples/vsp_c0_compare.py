51a5c281f62b2fd66ddd1c4b3acda635f739f9de
Trevor Laughlin <trevor@laughlinresearch.com>
2018-01-10 13:09:52 -0500

    Add derivative evaluation to OpenVSP C0 comparison

--- examples/vsp_c0_compare.py
+++ examples/vsp_c0_compare.py
@@ -1,6 +1,9 @@
+from numpy import linspace
+
 from afem.config import Settings
-from afem.exchange import StepImport
+from afem.exchange import StepImport, ImportVSP
 from afem.graphics import Viewer
+from afem.misc.check import pairwise
 from afem.topology import *
 
 Settings.log_to_console()
@@ -13,6 +16,10 @@ Settings.set_units('in')
 fn = r'../models/777-200LR_nosplit.stp'
 step = StepImport()
 step.read(fn)
+
+ImportVSP.step_file(fn, False)
+wing = ImportVSP.get_body('Wing')
+vsp_srf_35 = wing.get_metadata('vsp surface')
 
 v = Viewer()
 v.set_white_background()
@@ -33,6 +40,10 @@ fn = r'../models/777-200LR_nosplit_vsp315.stp'
 step = StepImport()
 step.read(fn)
 
+ImportVSP.step_file(fn, False)
+wing = ImportVSP.get_body('Wing')
+vsp_srf_315 = wing.get_metadata('vsp surface')
+
 v.display_shape(step.shape, (0.5, 0.5, 0.5))
 v.start()
 v.remove_all()
@@ -42,3 +53,29 @@ c0_shape = DivideC0Shape(step.shape).shape
 for f in ExploreShape.get_faces(c0_shape):
     v.display_shape(f)
 v.start()
+v.remove_all()
+
+# First derivatives ------------------------------------------------------------
+u_35 = vsp_srf_35.local_to_global_param('u', 0.5)
+u_315 = vsp_srf_315.local_to_global_param('u', 0.5)
+
+# Make sure curves are at same location
+u_iso_35 = vsp_srf_35.u_iso(u_35)
+u_iso_315 = vsp_srf_315.u_iso(u_315)
+v.add(u_iso_35, u_iso_315, vsp_srf_35)
+v.start()
+
+# Evaluate v-direction first derivative at 100 points between each knot value.
+# Record the v-parameter and magnitude to a csv file.
+n = 100
+fout = open('dv_35.csv', 'w')
+for v1, v2 in pairwise(vsp_srf_35.vknots):
+    for v in linspace(v1, v2, n):
+        dv = vsp_srf_35.deriv(u_35, v, 0, 1)
+        fout.write('{}, {}\n'.format(v, dv.mag))
+
+fout = open('dv_315.csv', 'w')
+for v1, v2 in pairwise(vsp_srf_315.vknots):
+    for v in linspace(v1, v2, n):
+        dv = vsp_srf_315.deriv(u_315, v, 0, 1)
+        fout.write('{}, {}\n'.format(v, dv.mag))
