4fd5427b7c1630831bff93c2c276163094025604
Trevor Laughlin <trevor@laughlinresearch.com>
2018-03-07 09:59:02 -0500

    Add CachedProperty class

---
+++ afem/misc/cache.py
@@ -0,0 +1,16 @@
+class CachedProperty(object):
+    """
+    Cached property.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
+
+
+cached_property = CachedProperty
