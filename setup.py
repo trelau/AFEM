d787257e8e12240977edea0617f186ae5a301934
Trevor Laughlin <trevor@laughlinresearch.com>
2018-03-08 21:15:49 -0500

    Update setup.py and remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-        name='afem',
-        version='0.2.0',
-        packages=find_packages('.'),
-        package_dir={'afem': 'afem'},
-        author='Laughlin Research, LLC',
-        author_email='info@laughlinresearch.com',
-        description='Airframe Finite Element Modeling',
-        license='Proprietary'
+    name='afem',
+    version='0.2.0',
+    packages=['afem', 'afem.exchange', 'afem.geometry', 'afem.graphics',
+              'afem.misc', 'afem.occ', 'afem.oml', 'afem.smesh',
+              'afem.structure', 'afem.topology'],
+    package_data={'afem': ['graphics/resources/*']},
+    author='Laughlin Research, LLC',
+    author_email='info@laughlinresearch.com',
+    description='Airframe Finite Element Modeling',
+    license='Proprietary'
 )
