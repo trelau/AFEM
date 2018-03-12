b5a4bfca93fb36e9d4f621453f3bd5c72b2aee6f
Trevor Laughlin <trevor@laughlinresearch.com>
2018-03-12 14:28:18 -0400

    Add afem.exchange.xde module

---
+++ afem/exchange/xde.py
@@ -0,0 +1,11 @@
+"""
+TDocStd_Document
+XCAFApp_Application
+XCAFDoc_ShapeTool
+
+TDF_Label XCAFDoc_ShapeTool::AddShape(const TopoDS_Shape & S, const Standard_Boolean makeAssembly=Standard_True, const Standard_Boolean makePrepare=Standard_True);
+Handle_TDataStd_Name TDataStd_Name::Set(const TDF_Label & label, const TCollection_ExtendedString & string);
+
+IGESCAFControl_Writer::WriteNames
+STEPCAFControl_Writer::WriteNames
+"""
