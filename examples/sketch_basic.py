2b6fb74e6bd73703b8d5dd647df17b1123b3c98a
Trevor Laughlin <trevor@laughlinresearch.com>
2018-03-30 11:56:29 -0400

    Add basic example for sketch package using airfoil file

---
+++ examples/sketch_basic.py
@@ -0,0 +1,28 @@
+from afem.geometry import *
+from afem.graphics import Viewer
+from afem.sketch import CrossSection
+from afem.topology import *
+
+# Create a new cross section
+cs = CrossSection()
+
+# Generate a 2-D profile by reading and approximating an airfoil file from the
+# UIUC database. Close the trailing edge if necessary.
+cs.read_uiuc('../models/clarky.dat', close=True)
+
+# Define a plane at the root and scale
+pln1 = PlaneByAxes(axes='xz').plane
+cs.build(pln1, scale=5)
+wire1 = cs.wires[0]
+
+# Define plane at the tip and rotate
+pln2 = PlaneByAxes((3, 15, 0), axes='xz').plane
+cs.build(pln2, scale=1.5, rotate=8)
+wire2 = cs.wires[0]
+
+# Use the wires to loft a solid
+shape = LoftShape([wire1, wire2], True).shape
+
+gui = Viewer()
+gui.add(wire1, wire2, shape)
+gui.start()
