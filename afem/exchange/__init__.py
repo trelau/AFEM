221a6682bab68582d2b91ecb8d3efcf64b0cefa4
Trevor Laughlin <trevor@laughlinresearch.com>
2018-04-02 11:52:13 -0400

    Update to open source license

--- afem/exchange/__init__.py
+++ afem/exchange/__init__.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# This file is part of AFEM which provides an engineering toolkit for airframe
+# finite element modeling during conceptual design.
 #
-# Copyright (c) 2017 Laughlin Research, L.L.C.
+# Copyright (C) 2016-2018  Laughlin Research, LLC (info@laughlinresearch.com)
 #
-# This file is subject to the license agreement that was delivered
-# with this source code.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# THE SOFTWARE AND INFORMATION ARE PROVIDED ON AN "AS IS" BASIS,
-# WITHOUT ANY WARRANTIES OR REPRESENTATIONS EXPRESS, IMPLIED OR 
-# STATUTORY; INCLUDING, WITHOUT LIMITATION, WARRANTIES OF QUALITY,
-# PERFORMANCE, MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 from afem.exchange.iges import IgesRead, IgesWrite
 from afem.exchange.step import StepWrite, StepRead
 from afem.exchange.stl import StlExport
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
-#!/usr/bin/env python
+# This file is part of AFEM which provides an engineering toolkit for airframe
+# finite element modeling during conceptual design.
+#
+# Copyright (C) 2016-2018  Laughlin Research, LLC (info@laughlinresearch.com)
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 from setuptools import setup
 
 setup(
     name='afem',
     version='0.2.0',
     packages=['afem', 'afem.exchange', 'afem.geometry', 'afem.graphics',
-              'afem.misc', 'afem.occ', 'afem.oml', 'afem.smesh',
+              'afem.misc', 'afem.occ', 'afem.oml', 'afem.sketch', 'afem.smesh',
               'afem.structure', 'afem.topology'],
     package_data={'afem': ['graphics/resources/*']},
     author='Laughlin Research, LLC',
     author_email='info@laughlinresearch.com',
-    description='Airframe Finite Element Modeling',
-    license='Proprietary'
+    description='Airframe Finite Element Modeler',
+    license='LGPL v2.1'
 )
