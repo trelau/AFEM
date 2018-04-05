9240e1f4d6b00efdad205a01e0d9ba90bf582fb9
Trevor Laughlin <trevor@laughlinresearch.com>
2018-04-05 00:24:19 -0400

    Define initial classes for shape wrappers

--- afem/topology/entities.py
+++ afem/topology/entities.py
@@ -20,11 +20,330 @@ from math import sqrt
 
 from OCCT.BRepBndLib import BRepBndLib
 from OCCT.Bnd import Bnd_Box
+from OCCT.TopAbs import TopAbs_ShapeEnum
+from OCCT.TopExp import TopExp_Explorer
+from OCCT.TopoDS import TopoDS
 
 from afem.geometry.check import CheckGeom
 from afem.geometry.entities import Point
+from afem.graphics.display import ViewableItem
 
-__all__ = ["BBox"]
+__all__ = ["Shape", "Vertex", "Edge", "Wire", "Face", "Shell", "Solid",
+           "Compound", "CompSolid",
+           "BBox"]
+
+
+class Shape(ViewableItem):
+    """
+    Shape.
+
+    :param OCCT.TopoDS.TopoDS_Shape shape: The underlying shape.
+    """
+
+    VERTEX = TopAbs_ShapeEnum.TopAbs_VERTEX
+    EDGE = TopAbs_ShapeEnum.TopAbs_EDGE
+    WIRE = TopAbs_ShapeEnum.TopAbs_WIRE
+    FACE = TopAbs_ShapeEnum.TopAbs_FACE
+    SHELL = TopAbs_ShapeEnum.TopAbs_SHELL
+    SOLID = TopAbs_ShapeEnum.TopAbs_SOLID
+    COMPOUND = TopAbs_ShapeEnum.TopAbs_COMPOUND
+    COMPSOLID = TopAbs_ShapeEnum.TopAbs_COMPSOLID
+
+    def __init__(self, shape):
+        super(Shape, self).__init__()
+
+        self._shape = shape
+
+    def __hash__(self):
+        """
+        Use the hash code of the shape.
+        """
+        return hash(self.hash_code)
+
+    @property
+    def object(self):
+        """
+        :return: The underlying shape.
+        :rtype: OCCT.TopoDS.TopoDS_Shape
+        """
+        return self._shape
+
+    @property
+    def hash_code(self):
+        """
+        :return: The hash code of the shape computed using the TShape and
+            Location. Orientation is not used. The upper limit is 99,999.
+        :rtype: int
+        """
+        return self.object.HashCode(99999)
+
+    @property
+    def is_null(self):
+        """
+        :return: *True* if shape is null, *False* if not.
+        :rtype: bool
+        """
+        return self.object.IsNull()
+
+    @property
+    def shape_type(self):
+        """
+        :return: The shape type.
+        :rtype: OCCT.TopAbs.TopAbs_ShapeEnum
+        """
+        return self.object.ShapeType()
+
+    @property
+    def is_vertex(self):
+        """
+        :return: *True* if a Vertex, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_VERTEX
+
+    @property
+    def is_edge(self):
+        """
+        :return: *True* if an Edge, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_EDGE
+
+    @property
+    def is_wire(self):
+        """
+        :return: *True* if a Wire, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_WIRE
+
+    @property
+    def is_face(self):
+        """
+        :return: *True* if a Face, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_FACE
+
+    @property
+    def is_shell(self):
+        """
+        :return: *True* if a Shell, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_SHELL
+
+    @property
+    def is_solid(self):
+        """
+        :return: *True* if a Solid, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_SOLID
+
+    @property
+    def is_compound(self):
+        """
+        :return: *True* if a Compound, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_COMPOUND
+
+    @property
+    def is_compsolid(self):
+        """
+        :return: *True* if a CompSolid, *False* if not.
+        :rtype: bool
+        """
+        return self.shape_type == TopAbs_ShapeEnum.TopAbs_COMPSOLID
+
+    @property
+    def closed(self):
+        """
+        :return: The closed flag of the shape.
+        :rtype: bool
+        """
+        return self.object.Closed()
+
+    @property
+    def infinite(self):
+        """
+        :return: The infinite flag of the shape.
+        :rtype: bool
+        """
+        return self.object.Infinite()
+
+    def reverse(self):
+        """
+        Reverse the orientation of the shape.
+
+        :return: None.
+        """
+        self.object.Reverse()
+
+    def is_partner(self, other):
+        """
+        Check if this shape shares the same TShape with the other. Locations
+        and Orientations may differ.
+
+        :param afem.topology.entities.Shape other: Other shape.
+
+        :return: *True* if partner, *False* if not.
+        :rtype: bool
+        """
+        return self.object.IsPartner(other.object)
+
+    def is_same(self, other):
+        """
+        Check if this shape shares the same TShape and Location with the other.
+        Orientations may differ.
+
+        :param afem.topology.entities.Shape other: Other shape.
+
+        :return: *True* if same, *False* if not.
+        :rtype: bool
+        """
+        return self.object.IsSame(other.object)
+
+    def is_equal(self, other):
+        """
+        Check if this shape is equal to the other. That is, they share the same
+        TShape, Location, and Orientation.
+
+        :param afem.topology.entities.Shape other: Other shape.
+
+        :return: *True* if equal, *False* if not.
+        :rtype: bool
+        """
+        return self.object.IsEqual(other.object)
+
+    def downcasted(self):
+        """
+        Convert this shape to a more specific type based on its shape type.
+
+        :return: The new shape. Returns itself if not converted.
+        :rtype: afem.topology.entities.Shape
+        """
+        if self.is_vertex:
+            v = TopoDS.Vertex_(self.object)
+            return Vertex(v)
+        if self.is_edge:
+            e = TopoDS.Edge_(self.object)
+            return Edge(e)
+        if self.is_wire:
+            w = TopoDS.Wire_(self.object)
+            return Wire(w)
+        if self.is_face:
+            f = TopoDS.Face_(self.object)
+            return Face(f)
+        if self.is_shell:
+            s = TopoDS.Shell_(self.object)
+            return Shell(s)
+        if self.is_solid:
+            s = TopoDS.Solid_(self.object)
+            return Solid(s)
+        if self.is_compound:
+            c = TopoDS.Compound_(self.object)
+            return Compound(c)
+        if self.is_compsolid:
+            c = TopoDS.CompSolid_(self.object)
+            return CompSolid(c)
+        return self
+
+    def get_shapes(self, type_):
+        """
+        Get sub-shapes of a specified type from the shape.
+
+        :param OCCT.TopAbs.TopAbs_ShapeEnum type_: The shape type.
+
+        :return: List of sub-shapes.
+        :rtype: list(afem.topology.entities.Shape)
+        """
+        explorer = TopExp_Explorer(self.object, type_)
+        shapes = []
+        while explorer.More():
+            si = Shape(explorer.Current()).downcasted()
+            is_unique = True
+            for s in shapes:
+                if s.is_same(si):
+                    is_unique = False
+                    break
+            if is_unique:
+                shapes.append(si)
+            explorer.Next()
+        return shapes
+
+
+class Vertex(Shape):
+    """
+    Vertex.
+    """
+
+    def __init__(self, vertex):
+        super(Vertex, self).__init__(vertex)
+
+
+class Edge(Shape):
+    """
+    Edge.
+    """
+
+    def __init__(self, edge):
+        super(Edge, self).__init__(edge)
+
+
+class Wire(Shape):
+    """
+    Wire.
+    """
+
+    def __init__(self, wire):
+        super(Wire, self).__init__(wire)
+
+
+class Face(Shape):
+    """
+    Face.
+    """
+
+    def __init__(self, face):
+        super(Face, self).__init__(face)
+
+
+class Shell(Shape):
+    """
+    Shell.
+    """
+
+    def __init__(self, shell):
+        super(Shell, self).__init__(shell)
+
+
+class Solid(Shape):
+    """
+    Solid.
+    """
+
+    def __init__(self, solid):
+        super(Solid, self).__init__(solid)
+
+
+class Compound(Shape):
+    """
+    Compound.
+    """
+
+    def __init__(self, compound):
+        super(Compound, self).__init__(compound)
+
+
+class CompSolid(Shape):
+    """
+    CompSolid.
+    """
+
+    def __init__(self, compsolid):
+        super(CompSolid, self).__init__(compsolid)
 
 
 class BBox(Bnd_Box):
